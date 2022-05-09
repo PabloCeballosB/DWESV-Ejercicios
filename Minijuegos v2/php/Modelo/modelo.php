3183fcd4d624169f208f3a692aacd913bfd5214f
PabloCeballosB <90768884+PabloCeballosB@users.noreply.github.com>
2022-05-09 13:07:22 +0200

    Falta Modificar

---
+++ Minijuegos v2/php/Modelo/modelo.php
@@ -0,0 +1,55 @@
+<?php
+
+  require_once('../config.php');
+
+  class Modelo{
+
+    private $conexion;
+    public $filas;
+    public $filaMod;
+
+    /*Funcion que se encarga de poder conectar con el servidor de base de datos*/
+    function __construct(){
+      $this -> conexion = new mysqli(servidorbd, usuario, contrasena, basedatos);
+    }
+
+    /*Funcion que se encarga de insertar los datos introducidos en alta.php*/
+    function insertar(){
+      $nombre = $_POST["nombre"];
+      $ruta = $_POST["ruta"];
+      if(!empty($_FILES['icono']['name'])){
+          $icono = $_FILES['icono']['name'];
+      }
+      else {
+        $icono = 'NULL';
+      }
+      $consulta = "INSERT INTO minijuegos(nombre, icono, ruta) VALUES ('".$nombre."', '".$icono."', '".$ruta."' );";
+      $this-> conexion -> query($consulta);
+    }
+
+    /*Funcion que lista todos los minijuegos introducidos*/
+    function listar(){
+      $consulta = "SELECT * FROM minijuegos;";
+      $resultado = $this -> conexion -> query($consulta);
+      while($fila = $resultado->fetch_array()){
+          $this -> filas[] = $fila;
+      }
+    }
+
+    /*Funcion que modifica el minijuego deseado*/
+    function modificar(){
+      $consulta = "SELECT * FROM minijuegos WHERE idMinijuego=".$_GET['idMini'].";";
+      $resultado = $this -> conexion -> query($consulta);
+      $filaMod = mysql_fetch_array($resultado);
+
+    }
+
+    /*Funcion que borra el minijuego deseado*/
+    function borrar(){
+      $consulta = "DELETE FROM minijuegos WHERE idMinijuego=".$_GET['idMini'].";";
+      $this -> conexion -> query($consulta);
+    }
+  }
+
+
+?>
---
+++ Minijuegos v2/php/Vista/borrar.php
@@ -0,0 +1,28 @@
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Alta Minijuegos</title>
+    <link rel="stylesheet" type="text/css" href="../../css/estilos.css">
+  </head>
+  <body>
+    <!--Div de la barra de navegacion con sus respectivas lineas-->
+    <div id="barraNavegacion">
+      <ul>
+        <li><a href="../../index.html">Inicio</a></li>
+        <li><a href="../Controlador/controlador.php?click=alta">Alta de Minijuegos</a></li>
+        <li><a href="../Controlador/controlador.php?click=lista">Lista de Minijuegos</a></li>
+        <li><a href="../Controlador/controlador.php?click=listaSelect">Lista Select</a></li>
+        <li><a href="../Controlador/controlador.php?click=listaCheckbox">Lista Checkbox</a></li>
+      </ul>
+    </div>
+
+    <!--Div del cuadro de seguridad para borrar minijuego-->
+    <div id="borrarFila">
+      <p>¿Estas seguro de que quieres borrar el minijuego?</p>
+      <form method="post" id="botones">
+          <input type="submit" name="cancelar" value="Cancelar" id="btnCancelar"/>
+          <input type="submit" name="aceptar" value="Aceptar" id="btnAceptar"/>
+      </form>
+    </div>
+  </body>
+</html>
